33b4f6a2c155cede0405ccdb719939c0b284b484
jqviy <79417961+jqviy@users.noreply.github.com>
2023-12-20 15:08:52 +0100

    TRABAJO FUNCIONAL YEAPAA!

--- src/cambiarcontraseña.vhd
+++ src/cambiarcontraseña.vhd
@@ -20,9 +20,11 @@ architecture Behavioral of fsm_Cambiar_contrasena is
     type STATES is (S0, S1, S2, S3, S4);
     signal current_state: STATES := S0;
     signal next_state: STATES;
+    signal current_codigo : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+    signal next_codigo : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
  
     signal num : STD_LOGIC_VECTOR (1 downto 0) := (others => '0');
-    signal codigo : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+
  
 begin
         --DONE1<='0';
@@ -32,92 +34,109 @@ begin
     -- Completar
        if(RESET='0' and modo='1')then
             current_state<=S0;
+            current_codigo <= (others => '0');
        elsif (rising_edge(CLK)) then
             current_state<=next_state;
+            current_codigo <= next_codigo;
        end if;
     end process;
  
     --Proceso de asignar nuevo estado
-    nextstate_decod: process (boton, current_state,modo)
+    nextstate_decod: process (boton, num, current_state, current_codigo, modo)
     begin
         if modo='1' then 
-            next_state <= current_state;
+           next_state  <= current_state;
+            next_codigo <= current_codigo;
             case current_state is
             when S0 =>
-                if (boton(0) = '1') or ( boton(1) = '1')or ( boton(2) = '1')or ( boton(3) = '1')   then
-                    next_state <= S1;
-            end if;
-            when S1 =>
-                if (boton(0) = '1') or ( boton(1) = '1')or ( boton(2) = '1')or ( boton(3) = '1')  then
-                    next_state <= S2;
+                if boton(3 downto 0) /= "0000"  then
+                    next_state  <= S1;
+                    next_codigo <= current_codigo(5 downto 0) & num;
                 elsif (boton(4) = '1') then
-                   next_state <= S0; 
+                    next_state  <= S0;
+                    next_codigo <= (others => '0');
+                end if;
+            when S1 =>
+                if boton(3 downto 0) /= "0000"  then
+                    next_state  <= S2;
+                    next_codigo <= current_codigo(5 downto 0) & num;
+                elsif (boton(4) = '1') then
+                    next_state  <= S0;
+                    next_codigo <= (others => '0');
                 end if;
             when S2 =>
-                if (boton(0) = '1') or ( boton(1) = '1')or ( boton(2) = '1')or ( boton(3) = '1')  then
-                    next_state <= S3;
+                if boton(3 downto 0) /= "0000"  then
+                    next_state  <= S3;
+                    next_codigo <= current_codigo(5 downto 0) & num;
                 elsif (boton(4) = '1') then
-                    next_state <= S0; 
+                    next_state  <= S0;
+                    next_codigo <= (others => '0');
                 end if;
             when S3 =>
-                if (boton(0) = '1') or ( boton(1) = '1')or ( boton(2) = '1')or ( boton(3) = '1') then
-                    next_state <= S4;
+                if boton(3 downto 0) /= "0000"  then
+                    next_state  <= S4;
+                    next_codigo <= current_codigo(5 downto 0) & num;
                 elsif (boton(4) = '1') then
-                    next_state <= S0;       
+                    next_state  <= S0;
+                    next_codigo <= (others => '0');
                 end if;
             when S4 =>
                 if (boton(4) = '1') then
-                    next_state <= S0;
+                    next_state  <= S0;
+                    next_codigo <= (others => '0');
                 end if;
             when others =>
-                next_state <= S0;
-            end case;  
-        elsif(modo='0') then 
-            next_state <= S0;   
-        end if;
-    
+                next_state  <= S0;
+                next_codigo <= (others => '0');
+            end case;   
+        end if; 
 end process;
 
 
-    --Proceso de salidas
-    output_decod: process (current_state, modo, boton)
+    -- Codificador pulsador
+    boton_encoder: process (boton)
     begin
-        if modo='1' then
-            --Asignamos cada botón a un valor
-            if (boton(0) = '1') then 
-                num <= "00";
-            elsif (boton(1) = '1') then
-                num <= "01";
-            elsif (boton(2) = '1') then 
-                num <= "10";
-            elsif (boton(3) = '1') then 
-                num <= "11";
-            elsif boton(4)='1' then 
-                num <= "XX";
-            end if;
-            
+        --Asignamos cada botón a un valor
+        if (boton(0) = '1') then 
+            num <= "00";
+        elsif (boton(1) = '1') then
+            num <= "01";
+        elsif (boton(2) = '1') then 
+            num <= "10";
+        elsif (boton(3) = '1') then 
+            num <= "11";
+        elsif boton(4)='1' then 
+            num <= "XX";
+        end if;  
+    end process;
+        
+    --Proceso de salidas
+    output_decod: process (boton, current_state, modo)
+    begin
+        if modo = '1' then
             case current_state is
-            when S0 =>
-                --Cambiar lo que pasemos, otra cosa distinta
-                DONE1<='0';
-                codigo<=(OTHERS=>'0');
-            when S1 =>
-                --DONE1<='0';
-                codigo <= num (1 downto 0) & codigo(5 downto 0);
-            when S2 =>
-                codigo <= codigo(7 downto 6) & num (1 downto 0) & codigo(3 downto 0);
-            when S3 =>
-                codigo <= codigo(7 downto 4) & num (1 downto 0) & codigo(1 downto 0);
-            when S4 =>
-                codigo <= codigo(7 downto 2) & num (1 downto 0);
-                new_code<=codigo;
-                DONE1<='1';
-            when others =>
-                codigo<=(OTHERS=>'0');
-            end case;
-            
-            --new_code<=codigo;
-         end if;   
+                when S0 =>
+                    --CAmbiar lo que pasemos, otra cosa distinta
+                    DONE1 <= '0';
+                
+                when S1 =>
+                    DONE1 <= '0';
+  
+                when S2 =>
+                    DONE1 <= '0';
+
+                when S3 =>
+                    DONE1 <= '0';
+
+                when S4 =>
+                    DONE1 <= '1';
+
+                when others =>
+                    DONE1 <= '0';
+            end case;        
+        end if;
     end process;
 
+    new_Code <= current_codigo;
+    
 end Behavioral;
--- src/desbloquear.vhd
+++ src/desbloquear.vhd
@@ -19,9 +19,10 @@ architecture Behavioral of fsm_Desbloquear is
     type STATES is (S0, S1, S2, S3, S4);
     signal current_state: STATES := S0;
     signal next_state: STATES;
+    signal current_codigo : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+    signal next_codigo : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
  
     signal num : STD_LOGIC_VECTOR (1 downto 0) := (others => '0');
-    signal codigo : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
     signal LED : STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
  
 begin
@@ -30,101 +31,117 @@ begin
     state_register: process (RESET, CLK, modo)
     begin
         -- Completar
-       if(RESET='0' and modo='0')then
-            current_state<=S0;
+       if RESET = '0' and modo = '0' then
+            current_state  <= S0;
+            current_codigo <= (others => '0');
        elsif (rising_edge(CLK)) then
-            current_state<=next_state;
+            current_state  <= next_state;
+            current_codigo <= next_codigo;
        end if;
     end process;
  
     --Proceso de asignar nuevo estado
-    nextstate_decod: process (boton, current_state,modo)
+    nextstate_decod: process (boton, num, current_state, current_codigo, modo)
     begin
         if modo='0' then 
-            next_state <= current_state;
+            next_state  <= current_state;
+            next_codigo <= current_codigo;
             case current_state is
             when S0 =>
-                if (boton(0) = '1') or ( boton(1) = '1')or ( boton(2) = '1')or ( boton(3) = '1')   then
-                    next_state <= S1;
+                if boton(3 downto 0) /= "0000"  then
+                    next_state  <= S1;
+                    next_codigo <= current_codigo(5 downto 0) & num;
                 elsif (boton(4) = '1') then
-                    next_state <= S0;
+                    next_state  <= S0;
+                    next_codigo <= (others => '0');
                 end if;
             when S1 =>
-                if (boton(0) = '1') or ( boton(1) = '1')or ( boton(2) = '1')or ( boton(3) = '1')  then
-                    next_state <= S2;
+                if boton(3 downto 0) /= "0000"  then
+                    next_state  <= S2;
+                    next_codigo <= current_codigo(5 downto 0) & num;
                 elsif (boton(4) = '1') then
-                    next_state <= S0;
+                    next_state  <= S0;
+                    next_codigo <= (others => '0');
                 end if;
             when S2 =>
-                if (boton(0) = '1') or ( boton(1) = '1')or ( boton(2) = '1')or ( boton(3) = '1')  then
-                    next_state <= S3;
+                if boton(3 downto 0) /= "0000"  then
+                    next_state  <= S3;
+                    next_codigo <= current_codigo(5 downto 0) & num;
                 elsif (boton(4) = '1') then
-                    next_state <= S0;
+                    next_state  <= S0;
+                    next_codigo <= (others => '0');
                 end if;
             when S3 =>
-                if (boton(0) = '1') or ( boton(1) = '1')or ( boton(2) = '1')or ( boton(3) = '1') then
-                    next_state <= S4;
+                if boton(3 downto 0) /= "0000"  then
+                    next_state  <= S4;
+                    next_codigo <= current_codigo(5 downto 0) & num;
                 elsif (boton(4) = '1') then
-                    next_state <= S0;
+                    next_state  <= S0;
+                    next_codigo <= (others => '0');
                 end if;
             when S4 =>
                 if (boton(4) = '1') then
-                    next_state <= S0;
+                    next_state  <= S0;
+                    next_codigo <= (others => '0');
                 end if;
             when others =>
-                next_state <= S0;
+                next_state  <= S0;
+                next_codigo <= (others => '0');
             end case;
         end if;
     end process;
 
-
+    -- Codificador pulsador
+    boton_encoder: process (boton)
+    begin
+        --Asignamos cada botón a un valor
+        if (boton(0) = '1') then 
+            num <= "00";
+        elsif (boton(1) = '1') then
+            num <= "01";
+        elsif (boton(2) = '1') then 
+            num <= "10";
+        elsif (boton(3) = '1') then 
+            num <= "11";
+        elsif boton(4)='1' then 
+            num <= "XX";
+        end if;  
+    end process;
+        
     --Proceso de salidas
     output_decod: process (boton, current_state, modo, LED)
     begin
-        if modo='0' then
-    
-            --Asignamos cada botón a un valor
-            if (boton(0) = '1') then 
-                num <= "00";
-            elsif (boton(1) = '1') then
-                num <= "01";
-            elsif (boton(2) = '1') then 
-                num <= "10";
-            elsif (boton(3) = '1') then 
-                num <= "11";
-            elsif boton(4)='1' then 
-                num <= "XX";
-            end if;  
-            
+        if modo = '0' then
             case current_state is
-            when S0 =>
-                --CAmbiar lo que pasemos, otra cosa distinta
-                DONE0<='0';
-                LED <= "0000";
-            
-                codigo <= (OTHERS => '0');
-            when S1 =>
-                --DONE0<='0';
-                LED <= "0001";
-                codigo <= num (1 downto 0) & codigo(5 downto 0);
-            when S2 =>
-                LED <= "0011";
-                codigo <= codigo(7 downto 6) & num (1 downto 0) & codigo(3 downto 0);
-            when S3 =>
-                LED <= "0111";
-                codigo <= codigo(7 downto 4) & num (1 downto 0) & codigo(1 downto 0);
-            when S4 =>
-                LED <= "1111";
-                codigo <= codigo(7 downto 2) & num (1 downto 0);
-                CODE_OUT <= codigo;
-                DONE0<='1';
-            when others =>
-            codigo <= (OTHERS => '0');
-            end case;
-            
-            --CODE_OUT <= codigo;
-            LED_OUT <= LED;
+                when S0 =>
+                    --CAmbiar lo que pasemos, otra cosa distinta
+                    DONE0 <= '0';
+                    LED   <= "0000";
+                
+                when S1 =>
+                    DONE0 <= '0';
+                    LED   <= "0001";
+  
+                when S2 =>
+                    DONE0 <= '0';
+                    LED   <= "0011";
+
+                when S3 =>
+                    DONE0 <= '0';
+                    LED   <= "0111";
+
+                when S4 =>
+                    DONE0 <= '1';
+                    LED   <= "1111";
+                    --CODE_OUT <= current_codigo;
+
+                when others =>
+                    DONE0 <= '0';
+                    LED   <= "0000";
+            end case;        
         end if;
     end process;
 
+    CODE_OUT <= current_codigo;
+    LED_OUT  <= LED;
 end Behavioral;
